eb91b58ed6e496223c650f603f53e3b19121f373
Paulína Palíková <paulipalik@gmail.com>
2021-04-24 22:34:38 +0200

    script2

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 'use strict';
+console.log('fungujem');
 
 let naTahu = 'circle';
+
 const policka = document.querySelectorAll('.policko');
+const kruzok = document.querySelector('.kruzok');
 
 for (let i = 0; i < policka.length; i++) {
   policka[i].addEventListener('click', () => {
     if (naTahu === 'circle') {
       policka[i].classList.add('board_field--circle');
-      document.querySelector('.kruzok').src = 'cross.svg';
+      policka[i].classList.add('zaplnene');
+      kruzok.src = 'cross.svg';
       naTahu = 'cross';
-      // policka[i].setAttribute('disabled', true);
     } else if (naTahu === 'cross') {
       policka[i].classList.add('board_field--cross');
-      document.querySelector('.kruzok').src = 'circle.svg';
+      policka[i].classList.add('zaplnene');
+      kruzok.src = 'circle.svg';
       naTahu = 'circle';
-      // policka[i].setAttribute('disabled', true);
     }
+  });
+}
+
+const zaplnene = document.querySelector('.zaplnene');
+for (let i = 0; i < policka.length; i++) {
+  policka[i].addEventListener('click', () => {
+    policka[i].disabled = true;
   });
 }
