a0866e0f9d4370f9757dc8494fec044c977670cb
Paulína Palíková <paulipalik@gmail.com>
2021-05-09 19:33:38 +0200

    Fix

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const kruzok = document.querySelector('.kruzok');
 for (let i = 0; i < policka.length; i++) {
   policka[i].addEventListener('click', () => {
     if (naTahu === 'circle') {
-      policka[i].classList.add('board_field--circle');
+      policka[i].classList.add('board__field--circle');
       policka[i].classList.add('zaplnene');
       kruzok.src = 'cross.svg';
       naTahu = 'cross';
     } else if (naTahu === 'cross') {
-      policka[i].classList.add('board_field--cross');
+      policka[i].classList.add('board__field--cross');
       policka[i].classList.add('zaplnene');
       kruzok.src = 'circle.svg';
       naTahu = 'circle';
