57e37c398fbddfc4093f8cb8669a11751b5e077e
Paulína Palíková <paulipalik@gmail.com>
2021-05-10 13:41:44 +0200

    GetSymbol-GetPosition-working

--- index.js
+++ index.js
@@ -43,10 +43,33 @@ const getSymbol = (polozka) => {
   // Název třídy přizpůsob tvému kódu.
   if (polozka.classList.contains('board__field--cross')) {
     return 'cross';
-  } else if (polozka.classList.contains) 'board__field--circle';
-  {
+  } else if (polozka.classList.contains('board__field--circle')) {
     return 'circle';
+  } else {
+    return 'undefined';
   }
 };
 
 console.log(getSymbol(policka[1]));
+
+const boardSize = 10; // 10x10
+
+const getField = (row, column) => {
+  return policka[row * boardSize + column];
+};
+
+console.log(getField(0, 9));
+
+const getPosition = (polozka) => {
+  let polozkaIndex = 0;
+  while (polozkaIndex < policka.length && polozka !== policka[polozkaIndex]) {
+    polozkaIndex++;
+  }
+
+  return {
+    row: Math.floor(polozkaIndex / boardSize),
+    column: polozkaIndex % boardSize,
+  };
+};
+
+console.log(getPosition(policka[70]));
